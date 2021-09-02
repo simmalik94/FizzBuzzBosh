48aec8b233442bb2ea68c1efb1be94ca688b6c2a
Usama Malik <usama@usamas-air.home>
2021-09-02 12:30:47 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { Grid, Paper, TextField, Button, Typography } from "@material-ui/core";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const paperStyle = {
+    backgroundColor: "lightBlue",
+    margin: "100px auto",
+    height: "20vh",
+    width: "300px",
+  };
+  const btnStyle = {
+    margin: "40px",
+  };
+  const fizz = num => {
+    if (num % 3 === 0 && num % 5 === 0 && num % 7 === 0) {
+      toast.success("FizzBuzzBosh!");
+    } else if (num % 5 === 0 && num % 7 === 0) {
+      toast.success("BuzzBosh!");
+    } else if (num % 3 === 0 && num % 7 === 0) {
+      toast.success("FizzBosh!");
+    } else if (num % 3 === 0 && num % 5 === 0) {
+      toast.success("FizzBuzz!");
+    } else if (num % 7 === 0) {
+      toast.success("Bosh!");
+    } else if (num % 5 === 0) {
+      toast.success("Buzz!");
+    } else if (num % 3 === 0) {
+      toast.success("Fizz!");
+    } else {
+      toast.error("None!");
+    }
+  };
+  const handleChange = e => {
+    setInput(e.target.value);
+  };
+  const handleClick = e => {
+    fizz(input);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <ToastContainer></ToastContainer>
+      <Grid>
+        <Typography
+          style={{
+            color: "red",
+            border: "1px solid",
+            width: "300px",
+            borderRadius: "5px",
+            margin: "10px auto",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          FIZBUZZBOSH!!
+        </Typography>
+        <Paper elevation={10} style={paperStyle}>
+          <TextField
+            label='Enter a Number!'
+            value={input}
+            fullWidth
+            onChange={handleChange}
+          >
+            Hello
+          </TextField>
+          <Button
+            variant='contained'
+            color='secondary'
+            style={btnStyle}
+            onClick={handleClick}
+          >
+            FizBuzzBosh!
+          </Button>
+        </Paper>
+      </Grid>
     </div>
   );
 }
